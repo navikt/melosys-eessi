331d3c7b68dda685ae8a5b08eadfd29ac8c2cf28
Andreas Strand <andreas.strand@nav.no>
2019-02-07 10:12:20 +0100

    initial commit

---
+++ src/main/java/no/nav/melosys/lovvalg/MelosysLovvalgApplication.java
@@ -0,0 +1,14 @@
+package no.nav.melosys.lovvalg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelosysLovvalgApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MelosysLovvalgApplication.class, args);
+  }
+
+}
+
