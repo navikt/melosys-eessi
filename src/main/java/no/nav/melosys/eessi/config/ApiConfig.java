85d2a880474b98a6e4005beab2b49b9a8e5bf83e
Andreas Strand <andreas.strand@nav.no>
2019-02-20 14:11:31 +0100

    Sett api-header kun ved profil 'nais'

--- src/main/java/no/nav/melosys/eessi/config/ApiConfig.java
+++ src/main/java/no/nav/melosys/eessi/config/ApiConfig.java
@@ -3,10 +3,12 @@ package no.nav.melosys.eessi.config;
 import no.nav.melosys.eessi.security.HeaderValidatorRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Profile("nais")
 @Configuration
 public class ApiConfig implements WebMvcConfigurer {
 
