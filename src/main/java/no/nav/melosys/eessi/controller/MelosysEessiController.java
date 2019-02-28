7e9a8616d3713640763b2f23f9c67e12fda16e34
Andreas Strand <andreas.strand@nav.no>
2019-02-28 17:06:39 +0100

    Exceptions annotert med @Responsestatus blir ikke logget. Logger manuelt i controller

--- src/main/java/no/nav/melosys/eessi/controller/MelosysEessiController.java
+++ src/main/java/no/nav/melosys/eessi/controller/MelosysEessiController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.melosys.eessi.controller.dto.SedDataDto;
+import no.nav.melosys.eessi.service.joark.OpprettUtgaaendeJournalpostService;
 import no.nav.melosys.eessi.service.sed.SedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,17 +20,24 @@ public class MelosysEessiController {
     private final SedService sedService;
 
     @Autowired
-    public MelosysEessiController(SedService sedService) {
+    public MelosysEessiController(SedService sedService,
+            OpprettUtgaaendeJournalpostService opprettUtgaaendeJournalpostService) {
         this.sedService = sedService;
     }
 
     @PostMapping("/createAndSend")
     public Map<String, String> createAndSendCase(@RequestBody SedDataDto sedDataDto) throws Exception {
         log.info("/api/createAndSend: Oppretter ny buc og sed");
-        String rinaCaseId = sedService.createAndSend(sedDataDto);
-        Map<String, String> result = Maps.newHashMap();
-        result.put("rinaCaseId", rinaCaseId);
 
-        return result;
+        try {
+            String rinaCaseId = sedService.createAndSend(sedDataDto);
+            Map<String, String> result = Maps.newHashMap();
+            result.put("rinaCaseId", rinaCaseId);
+            return result;
+        } catch (Exception e) {
+            log.error("Error in /sed/createAndSend", e);
+            throw e;
+        }
+
     }
 }
