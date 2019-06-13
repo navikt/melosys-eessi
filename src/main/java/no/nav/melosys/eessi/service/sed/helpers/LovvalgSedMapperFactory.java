4011a3ad3ebf1500fa2d11d6b4a123c4f872ac08
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-06-13 15:00:12 +0200

    Legger til en mapper for A005

--- src/main/java/no/nav/melosys/eessi/service/sed/helpers/LovvalgSedMapperFactory.java
+++ src/main/java/no/nav/melosys/eessi/service/sed/helpers/LovvalgSedMapperFactory.java
@@ -4,10 +4,7 @@ import java.util.EnumMap;
 import java.util.Map;
 import no.nav.melosys.eessi.models.SedType;
 import no.nav.melosys.eessi.models.exception.MappingException;
-import no.nav.melosys.eessi.service.sed.mapper.A001Mapper;
-import no.nav.melosys.eessi.service.sed.mapper.A008Mapper;
-import no.nav.melosys.eessi.service.sed.mapper.A009Mapper;
-import no.nav.melosys.eessi.service.sed.mapper.LovvalgSedMapper;
+import no.nav.melosys.eessi.service.sed.mapper.*;
 
 public class LovvalgSedMapperFactory {
 
@@ -15,6 +12,7 @@ public class LovvalgSedMapperFactory {
 
     static {
         sedMappers.put(SedType.A001, A001Mapper.class);
+        sedMappers.put(SedType.A005, A005Mapper.class);
         sedMappers.put(SedType.A008, A008Mapper.class);
         sedMappers.put(SedType.A009, A009Mapper.class);
     }
