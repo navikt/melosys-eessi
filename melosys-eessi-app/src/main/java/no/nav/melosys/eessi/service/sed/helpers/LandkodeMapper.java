b3c4fb1eb0c19777fc51c5e57da5ad7d18e16d7e
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-08-16 10:02:09 +0200

    Trenger null-sjekker for mappers likevel

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/sed/helpers/LandkodeMapper.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/sed/helpers/LandkodeMapper.java
@@ -44,6 +44,10 @@ public class LandkodeMapper {
     }
 
     public static String getLandkodeIso2(String landkodeIso3) throws NotFoundException {
+        if (landkodeIso3 == null) {
+            return null;
+        }
+
         if (landkodeIso3.length() == 2) {
             return landkodeIso3;
         }
