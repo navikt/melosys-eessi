62d7d0615c3e4958ab8a6f99d8219e65b052e6fd
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-09-27 09:41:33 +0200

    Fjerner en unødvendig metode i A002Mapper

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/sed/mapper/lovvalg/A002Mapper.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/sed/mapper/lovvalg/A002Mapper.java
@@ -5,28 +5,14 @@ import no.nav.melosys.eessi.controller.dto.SedDataDto;
 import no.nav.melosys.eessi.controller.dto.SvarAnmodningUnntakDto;
 import no.nav.melosys.eessi.models.SedType;
 import no.nav.melosys.eessi.models.exception.MappingException;
-import no.nav.melosys.eessi.models.sed.SED;
 import no.nav.melosys.eessi.models.sed.medlemskap.impl.MedlemskapA002;
 import no.nav.melosys.eessi.models.sed.medlemskap.impl.SvarAnmodningUnntakBeslutning;
 import no.nav.melosys.eessi.models.sed.medlemskap.impl.UnntakA002;
 import no.nav.melosys.eessi.models.sed.medlemskap.impl.VedtakA002;
 import no.nav.melosys.eessi.models.sed.nav.Fastperiode;
 import no.nav.melosys.eessi.models.sed.nav.Periode;
-import static no.nav.melosys.eessi.models.sed.Constants.SED_G_VER;
-import static no.nav.melosys.eessi.models.sed.Constants.SED_VER;
 
 public class A002Mapper implements LovvalgSedMapper<MedlemskapA002> {
-
-    public SED mapFraSed(SED sed, String begrunnelse, SvarAnmodningUnntakBeslutning resultat, LocalDate delvisInnvilgetFom, LocalDate delvisInnvilgetTom) {
-        SED a002 = new SED();
-        a002.setSed(SedType.A002.toString());
-        a002.setSedGVer(SED_G_VER);
-        a002.setSedVer(SED_VER);
-        a002.setNav(sed.getNav());
-        a002.setMedlemskap(getMedlemskap(begrunnelse, resultat, delvisInnvilgetFom, delvisInnvilgetTom));
-
-        return a002;
-    }
 
     @Override
     public MedlemskapA002 getMedlemskap(SedDataDto sedData) throws MappingException {
@@ -36,17 +22,14 @@ public class A002Mapper implements LovvalgSedMapper<MedlemskapA002> {
             throw new MappingException("Trenger SvarAnmodningUnntak for å opprette A002");
         }
 
-        return getMedlemskap(
+        MedlemskapA002 medlemskapA002 = new MedlemskapA002();
+        medlemskapA002.setUnntak(getUnntak(
                 svarAnmodningUnntak.getBegrunnelse(),
                 svarAnmodningUnntak.getBeslutning(),
                 svarAnmodningUnntak.getDelvisInnvilgetPeriode().getFom(),
                 svarAnmodningUnntak.getDelvisInnvilgetPeriode().getTom()
-        );
-    }
+        ));
 
-    private MedlemskapA002 getMedlemskap(String begrunnelse, SvarAnmodningUnntakBeslutning resultat, LocalDate delvisInnvilgetFom, LocalDate delvisInnvilgetTom) {
-        MedlemskapA002 medlemskapA002 = new MedlemskapA002();
-        medlemskapA002.setUnntak(getUnntak(begrunnelse, resultat, delvisInnvilgetFom, delvisInnvilgetTom));
         return medlemskapA002;
     }
 
