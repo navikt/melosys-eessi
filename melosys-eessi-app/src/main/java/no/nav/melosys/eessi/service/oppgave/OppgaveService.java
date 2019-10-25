aee5746524ca33eb53aecd6084e87b29863cb156
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2019-10-25 12:55:53 +0200

    Oppretter utgående journalføringsoppgave på sendt sed som ikke har sak

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/oppgave/OppgaveService.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/oppgave/OppgaveService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.melosys.eessi.integration.oppgave.OppgaveConsumer;
 import no.nav.melosys.eessi.integration.oppgave.OppgaveDto;
 import no.nav.melosys.eessi.integration.oppgave.OpprettOppgaveResponseDto;
+import no.nav.melosys.eessi.kafka.consumers.SedHendelse;
 import no.nav.melosys.eessi.models.exception.IntegrationException;
 import org.springframework.stereotype.Service;
 import static no.nav.melosys.eessi.service.sed.SedTypeTilTemaMapper.temaForSedType;
@@ -14,7 +15,10 @@ import static no.nav.melosys.eessi.service.sed.SedTypeTilTemaMapper.temaForSedTy
 public class OppgaveService {
 
     private static final String JFR = "JFR";
+    private static final String JFR_UT = "JFR_UT";
     private static final String ENHET_ID_FORDELING = "4303";
+    private static final String ENHET_MEDLEMSKAP_OG_AVGIFT = "4530";
+    private static final String PRIORITET_NORMAL = "NORM";
     private static final String BESKRIVELSE = "EØS - %s";
 
     private final OppgaveConsumer oppgaveConsumer;
@@ -29,7 +33,7 @@ public class OppgaveService {
                 .fristFerdigstillelse(LocalDate.now().plusWeeks(2L))
                 .journalpostId(journalpostID)
                 .oppgavetype(JFR)
-                .prioritet("NORM")
+                .prioritet(PRIORITET_NORMAL)
                 .tema(temaForSedType(sedType))
                 .tildeltEnhetsnr(ENHET_ID_FORDELING)
                 .beskrivelse(String.format(BESKRIVELSE, sedType))
@@ -38,5 +42,28 @@ public class OppgaveService {
         OpprettOppgaveResponseDto response = oppgaveConsumer.opprettOppgave(oppgaveDto);
         log.info("Oppgave til ID og fordeling opprettet med id {}", response.getId());
         return response.getId();
+    }
+
+    public String opprettUtgåendeJfrOppgave(String journalpostID, SedHendelse sedHendelse, String aktørId, String rinaUrl) throws IntegrationException {
+        OppgaveDto oppgaveDto = OppgaveDto.builder()
+                .aktivDato(LocalDate.now())
+                .beskrivelse(lagBeskrivelseUtgåendeJfrOppgave(sedHendelse, rinaUrl))
+                .fristFerdigstillelse(LocalDate.now().plusDays(7L))
+                .journalpostId(journalpostID)
+                .oppgavetype(JFR_UT)
+                .prioritet(PRIORITET_NORMAL)
+                .tema(temaForSedType(sedHendelse.getSedType()))
+                .tildeltEnhetsnr(ENHET_MEDLEMSKAP_OG_AVGIFT)
+                .aktoerId(aktørId)
+                .build();
+
+        OpprettOppgaveResponseDto response = oppgaveConsumer.opprettOppgave(oppgaveDto);
+        log.info("Utgående journalføringsoppgave opprettet med id {}", response.getId());
+        return response.getId();
+    }
+
+    private static String lagBeskrivelseUtgåendeJfrOppgave(SedHendelse sedHendelse, String rinaUrl) {
+        return String.format("%s, rinaSakId=%s, rinaDokumentId=%s, link=%s",
+                sedHendelse.getSedType(), sedHendelse.getRinaSakId(), sedHendelse.getRinaDokumentId(), rinaUrl);
     }
 }
--- melosys-eessi-app/src/test/java/no/nav/melosys/eessi/service/oppgave/OppgaveServiceTest.java
+++ melosys-eessi-app/src/test/java/no/nav/melosys/eessi/service/oppgave/OppgaveServiceTest.java
@@ -3,6 +3,7 @@ package no.nav.melosys.eessi.service.oppgave;
 import no.nav.melosys.eessi.integration.oppgave.OppgaveConsumer;
 import no.nav.melosys.eessi.integration.oppgave.OppgaveDto;
 import no.nav.melosys.eessi.integration.oppgave.OpprettOppgaveResponseDto;
+import no.nav.melosys.eessi.kafka.consumers.SedHendelse;
 import no.nav.melosys.eessi.models.SedType;
 import no.nav.melosys.eessi.models.exception.IntegrationException;
 import org.junit.Before;
@@ -47,5 +48,29 @@ public class OppgaveServiceTest {
         assertThat(oppgaveDto.getTema()).isEqualTo("UFM");
         assertThat(oppgaveDto.getOppgavetype()).isEqualTo("JFR");
         assertThat(oppgaveDto.getBeskrivelse()).isEqualTo("EØS - A009");
+    }
+
+    @Test
+    public void opprettUtgåendeJfrOppgave_validerFelter() throws IntegrationException {
+        when(oppgaveConsumer.opprettOppgave(any())).thenReturn(new OpprettOppgaveResponseDto());
+        String journalpostID = "111";
+        String aktørID = "321";
+        String rinaUrl = "https://test.local";
+        SedHendelse sedHendelse = SedHendelse.builder()
+                .sedType("A009")
+                .rinaSakId("12345")
+                .rinaDokumentId("deadbeef")
+                .build();
+
+        oppgaveService.opprettUtgåendeJfrOppgave(journalpostID, sedHendelse, aktørID, rinaUrl);
+
+        verify(oppgaveConsumer).opprettOppgave(captor.capture());
+
+        OppgaveDto oppgaveDto = captor.getValue();
+        assertThat(oppgaveDto.getJournalpostId()).isEqualTo(journalpostID);
+        assertThat(oppgaveDto.getTema()).isEqualTo("UFM");
+        assertThat(oppgaveDto.getOppgavetype()).isEqualTo("JFR_UT");
+        assertThat(oppgaveDto.getAktoerId()).isEqualTo(aktørID);
+        assertThat(oppgaveDto.getBeskrivelse()).contains("A009", "deadbeef");
     }
 }
