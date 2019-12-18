c6774cf008c9bc254911f42a45c75e82ac940432
Andreas Strand <andreas.strand@nav.no>
2019-12-18 23:40:53 +0100

    Fjerner prefix /api på servlet, og setter den prefiksen kun for endepunkt eksponert fra controller-pakke fra en RestController

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/config/ApiConfig.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/config/ApiConfig.java
@@ -1,16 +1,20 @@
 package no.nav.melosys.eessi.config;
 
+import java.util.Arrays;
+
 import no.nav.melosys.eessi.security.HeaderValidatorRequestInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Profile("nais")
 @Configuration
 public class ApiConfig implements WebMvcConfigurer {
+
+    private static final String NAIS = "nais";
+    private static final String API_PREFIX = "/api";
 
     private final Environment environment;
 
@@ -18,8 +22,25 @@ public class ApiConfig implements WebMvcConfigurer {
         this.environment = environment;
     }
 
-    @Bean
-    public HeaderValidatorRequestInterceptor requestInterceptor() {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix(API_PREFIX, ApiConfig::erApiTjeneste);
+    }
+
+    private static boolean erApiTjeneste(Class clazz) {
+        return clazz.getPackageName().startsWith(Konstanter.CONTROLLER_PAKKE)
+                && clazz.isAnnotationPresent(RestController.class);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        if (naisProfil()) {
+            registry.addInterceptor(requestInterceptor())
+                    .addPathPatterns("/api/**");
+        }
+    }
+
+    private HeaderValidatorRequestInterceptor requestInterceptor() {
         return new HeaderValidatorRequestInterceptor(
                 environment.getProperty("melosys.api.security.header-name"),
                 environment.getProperty("melosys.api.security.header-value"),
@@ -27,14 +48,7 @@ public class ApiConfig implements WebMvcConfigurer {
         );
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(requestInterceptor())
-                .addPathPatterns(
-                        "/**"
-                )
-                .excludePathPatterns(
-                        "/internal/**"
-                );
+    private boolean naisProfil() {
+        return Arrays.stream(environment.getActiveProfiles()).anyMatch(NAIS::equalsIgnoreCase);
     }
 }
---
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/config/Konstanter.java
@@ -0,0 +1,8 @@
+package no.nav.melosys.eessi.config;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+class Konstanter {
+    static final String CONTROLLER_PAKKE = "no.nav.melosys.eessi.controller";
+}
