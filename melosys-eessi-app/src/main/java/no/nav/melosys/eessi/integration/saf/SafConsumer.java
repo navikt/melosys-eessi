eda006d45234ef4f4103f9143e35df0f7e1c7e06
Andreas Strand <andreas.strand@nav.no>
2020-02-17 15:24:33 +0100

    Integrerer mot SAF for å kunne hente ut tilleggsopplysninger for journalpost

---
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/integration/saf/SafConsumer.java
@@ -0,0 +1,49 @@
+package no.nav.melosys.eessi.integration.saf;
+
+import java.util.Optional;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.melosys.eessi.integration.RestConsumer;
+import no.nav.melosys.eessi.integration.saf.dto.GraphQLResponse;
+import no.nav.melosys.eessi.models.exception.IntegrationException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Component
+public class SafConsumer implements RestConsumer {
+
+    private final RestTemplate restTemplate;
+
+    private static final String QUERY = "{query: journalpost(journalpostId: \"%s\") {tilleggsopplysninger{nokkel verdi}}}";
+
+    public SafConsumer(RestTemplate safRestTemplate) {
+        this.restTemplate = safRestTemplate;
+    }
+
+    public Optional<String> hentRinasakForJournalpost(String journalpostID) throws IntegrationException {
+
+        HttpEntity httpEntity = new HttpEntity(new GraphQLWrapper(String.format(QUERY, journalpostID), null), defaultHeaders());
+        GraphQLResponse response = restTemplate.exchange("/graphql", HttpMethod.POST, httpEntity, GraphQLResponse.class).getBody();
+
+        if (response == null) {
+            log.info("Mottatt null-response fra SAF");
+            return Optional.empty();
+        } else if (response.harFeil()) {
+            throw new IntegrationException("Feil ved integrasjon mot saf. Feilmeldinger: " + response.lagErrorString());
+        }
+
+        return response.getData().getQuery().hentRinaSakId();
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class GraphQLWrapper {
+        private String query;
+        private String variables;
+    }
+}
---
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/integration/saf/dto/Tilleggsopplysning.java
@@ -0,0 +1,13 @@
+package no.nav.melosys.eessi.integration.saf.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tilleggsopplysning {
+    private String nokkel;
+    private String verdi;
+}
