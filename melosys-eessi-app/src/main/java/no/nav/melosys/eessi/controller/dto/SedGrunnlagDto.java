fa9511f984a92fb9a2a2416574bff79ae915a396
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2020-02-28 11:00:31 +0100

    nullableStream for tryggere stream-håndtering

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/controller/dto/SedGrunnlagDto.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/controller/dto/SedGrunnlagDto.java
@@ -1,12 +1,16 @@
 package no.nav.melosys.eessi.controller.dto;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import lombok.Data;
 import no.nav.melosys.eessi.models.sed.nav.Arbeidsgiver;
 import no.nav.melosys.eessi.models.sed.nav.Nav;
 import no.nav.melosys.eessi.models.sed.nav.Pin;
+import no.nav.melosys.eessi.models.sed.nav.Selvstendig;
 
 @Data
 public class SedGrunnlagDto {
@@ -23,7 +27,7 @@ public class SedGrunnlagDto {
         sedGrunnlagDto.setUtenlandskIdent(mapUtenlandskIdent(nav.getBruker().getPerson().getPin()));
         sedGrunnlagDto.setArbeidssteder(mapArbeidssteder(nav.getArbeidssted()));
         sedGrunnlagDto.setArbeidsgivendeVirksomheter(mapVirksomheter(nav.getArbeidsgiver()));
-        sedGrunnlagDto.setSelvstendigeVirksomheter(mapVirksomheter(nav.getSelvstendig().getArbeidsgiver()));
+        sedGrunnlagDto.setSelvstendigeVirksomheter(mapSelvstendig(nav.getSelvstendig()));
 
         return sedGrunnlagDto;
     }
@@ -40,18 +44,30 @@ public class SedGrunnlagDto {
     }
 
     private static Adresse mapAdresse(List<no.nav.melosys.eessi.models.sed.nav.Adresse> adresser) {
-        return adresser.stream().findFirst().map(Adresse::av).orElseGet(Adresse::new);
+        return nullableStream(adresser).findFirst().map(Adresse::av).orElseGet(Adresse::new);
     }
 
     private static List<Ident> mapUtenlandskIdent(List<Pin> pins) {
-        return pins.stream().map(Ident::av).filter(Ident::erUtenlandsk).collect(Collectors.toList());
+        return nullableStream(pins).map(Ident::av).filter(Ident::erUtenlandsk).collect(Collectors.toList());
     }
 
     private static List<Arbeidssted> mapArbeidssteder(List<no.nav.melosys.eessi.models.sed.nav.Arbeidssted> arbeidssted) {
-        return arbeidssted.stream().map(Arbeidssted::av).collect(Collectors.toList());
+        return nullableStream(arbeidssted).map(Arbeidssted::av).collect(Collectors.toList());
     }
 
     private static List<Virksomhet> mapVirksomheter(List<Arbeidsgiver> arbeidsgivere) {
-        return arbeidsgivere.stream().map(Virksomhet::av).collect(Collectors.toList());
+        return nullableStream(arbeidsgivere).map(Virksomhet::av).collect(Collectors.toList());
+    }
+
+    private static List<Virksomhet> mapSelvstendig(Selvstendig selvstendig) {
+        return Optional.ofNullable(selvstendig).stream()
+                .map(Selvstendig::getArbeidsgiver)
+                .flatMap(Collection::stream)
+                .map(Virksomhet::av)
+                .collect(Collectors.toList());
+    }
+
+    private static <T> Stream<T> nullableStream(Collection<T> collection) {
+        return Optional.ofNullable(collection).stream().flatMap(Collection::stream);
     }
 }
