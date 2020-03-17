1ab20cb5f9f90b7feae6b8c6903a921eaed2153f
Andreas Strand <andreas.strand@nav.no>
2020-03-17 17:27:40 +0100

    Legger til variabel for feature-toggling av Norge sin eessi-ready status på enkelte BUC-er. Det skal returneres en tom liste ved henting av mottakinstitusjoner når Norge ikke er påkoblet den aktuelle BUCen

--- melosys-eessi-app/src/test/java/no/nav/melosys/eessi/service/eux/EuxServiceTest.java
+++ melosys-eessi-app/src/test/java/no/nav/melosys/eessi/service/eux/EuxServiceTest.java
@@ -49,7 +49,7 @@ public class EuxServiceTest {
     @Before
     public void setup() throws IOException, IntegrationException {
         final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        euxService = new EuxService(euxConsumer, bucMetrikker, RINA_MOCK_URL);
+        euxService = new EuxService(euxConsumer, bucMetrikker, RINA_MOCK_URL, "false");
 
         when(euxConsumer.opprettBucOgSed(anyString(), anyString(), any()))
                 .thenReturn(ImmutableMap.of(
@@ -178,7 +178,7 @@ public class EuxServiceTest {
 
     @Test
     public void hentMottakerinstitusjoner_sBuc18LandSverige_forventIngenInstitusjoner() throws IntegrationException {
-        List<Institusjon> institusjoner = euxService.hentMottakerinstitusjoner("S_BUC_18", "SE");
+        List<Institusjon> institusjoner = euxService.hentMottakerinstitusjoner("S_BUC_24", "SE");
         assertThat(institusjoner).isEmpty();
     }
 
@@ -200,6 +200,15 @@ public class EuxServiceTest {
         Institusjon institusjon = institusjoner.get(0);
         assertThat(institusjon.getAkronym()).isEqualTo("FK EL-TITTEI");
         assertThat(institusjon.getLandkode()).isEqualTo("GR");
+    }
+
+    @Test
+    public void hentMottakerinstitusjoner_laBuc02LandGRNorgeIkkePåkoblet_forventTomListe() throws IntegrationException {
+        euxService = new EuxService(euxConsumer, bucMetrikker, "url", "true");
+        List<Institusjon> institusjoner = euxService.hentMottakerinstitusjoner("LA_BUC_02", "GR");
+        assertThat(institusjoner).hasSize(0);
+
+        verify(euxConsumer, never()).hentInstitusjoner(any(), any());
     }
 
     @Test
