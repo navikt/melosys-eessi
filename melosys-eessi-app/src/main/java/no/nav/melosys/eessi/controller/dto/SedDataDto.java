b82f7dc8c5da4109a7810a3274c968f7e71b8f14
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2020-03-20 17:16:51 +0100

    JsonIgnoreProperties trengs likevel...

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/controller/dto/SedDataDto.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/controller/dto/SedDataDto.java
@@ -2,11 +2,13 @@ package no.nav.melosys.eessi.controller.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SedDataDto extends SedGrunnlagDto {
     //Persondok.
     private List<FamilieMedlem> familieMedlem;
--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/controller/dto/SedGrunnlagA003Dto.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/controller/dto/SedGrunnlagA003Dto.java
@@ -2,6 +2,7 @@ package no.nav.melosys.eessi.controller.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SedGrunnlagA003Dto extends SedGrunnlagDto {
     private List<Bestemmelse> overgangsregelbestemmelser;
     private List<Virksomhet> norskeArbeidsgivendeVirksomheter;
