5a9e91fa8f8e58c01c4a348d0d03f084a9098a0d
Andreas Strand <andreas.strand@nav.no>
2021-02-22 16:35:28 +0100

    Navn og fødselsopplysninger er også lister i PDL

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/integration/pdl/dto/PDLPerson.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/integration/pdl/dto/PDLPerson.java
@@ -6,8 +6,8 @@ import lombok.Data;
 
 @Data
 public class PDLPerson {
-    private PDLNavn navn;
-    private PDLFoedsel foedsel;
+    private Collection<PDLNavn> navn;
+    private Collection<PDLFoedsel> foedsel;
     private Collection<PDLStatsborgerskap> statsborgerskap;
     private Collection<PDLFolkeregisterPersonstatus> folkeregisterpersonstatus;
 }
