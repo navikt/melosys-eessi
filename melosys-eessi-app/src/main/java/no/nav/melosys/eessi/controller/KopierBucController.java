35a2b385f2351795c3b2aee000b55dcb7401cb35
Andreas Strand <andreas.strand@nav.no>
2021-05-27 20:43:06 +0200

    Funksjonalitet for å kopiere første SED i en buc

---
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/controller/KopierBucController.java
@@ -0,0 +1,36 @@
+package no.nav.melosys.eessi.controller;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import no.nav.melosys.eessi.service.buc.KopierBucService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KopierBucController {
+
+    private final KopierBucService kopierBucService;
+
+    public KopierBucController(KopierBucService kopierBucService) {
+        this.kopierBucService = kopierBucService;
+    }
+
+    @PostMapping("/bucer/kopier")
+    public Map<String, String> kopierOverBUCer(Collection<String> bucer) {
+        var saker = new HashMap<String, String>();
+        for (var rinaSaksnummer : bucer) {
+            String resultat;
+            try {
+                resultat = kopierBucService.kopierBUC(rinaSaksnummer);
+            } catch (Exception e) {
+                resultat = e.getMessage();
+            }
+
+            saker.put(rinaSaksnummer, resultat);
+        }
+
+        return saker;
+    }
+}
