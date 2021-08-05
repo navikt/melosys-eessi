802f19dbf0d58af3d33035ddebd8f2f0b6d682c6
Andreas Strand <andreas.strand@nav.no>
2021-08-05 23:20:13 +0200

    Skrev OppgaveConsumerTest vekk fra å bruke Spring sin MockRestServiceServer til okhttp sin MockWebServer i håp om å gjenskape en feil med PATCH-requests. Feilen blir ikke gjenskapt, men å bruke okhttp over spring for å teste en REST-integrasjon er uansett en forbedring

--- melosys-eessi-app/src/test/java/no/nav/melosys/eessi/integration/oppgave/OppgaveConsumerTest.java
+++ melosys-eessi-app/src/test/java/no/nav/melosys/eessi/integration/oppgave/OppgaveConsumerTest.java
@@ -1,80 +1,81 @@
 package no.nav.melosys.eessi.integration.oppgave;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.Optional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpMethod;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 class OppgaveConsumerTest {
-
-    private MockRestServiceServer server;
-    private final RestTemplate restTemplate = new RestTemplate();
 
     private OppgaveConsumer oppgaveConsumer;
 
     private final String OPPGAVE_ID = "123";
 
+    private static MockWebServer mockWebServer;
+    private static String rootUri;
+
+    @BeforeAll
+    static void setupAll() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+        rootUri = String.format("http://localhost:%s", mockWebServer.getPort());
+    }
+
     @BeforeEach
     public void setUp() {
-        server = MockRestServiceServer.createServer(restTemplate);
+        RestTemplate restTemplate = new RestTemplateBuilder().rootUri(rootUri).build();
         oppgaveConsumer = new OppgaveConsumer(restTemplate);
     }
 
     @Test
-    void hentOppgave_oppgaveFinnes_verifiserMapping() {
-        server.expect(requestTo("/oppgaver/" + OPPGAVE_ID))
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(withSuccess().contentType(MediaType.APPLICATION_JSON).body(hentOppgaveResponse()));
+    void hentOppgave_oppgaveFinnes_verifiserMapping() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setBody(hentOppgaveResponse())
+                        .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        );
+
         var oppgave = oppgaveConsumer.hentOppgave(OPPGAVE_ID);
-
-        assertThat(oppgave)
-                .extracting(
-                        HentOppgaveDto::getStatuskategori,
-                        HentOppgaveDto::getAktoerId,
-                        HentOppgaveDto::getTema
-                ).containsExactly(
-                "AAPEN",
-                "1332607802528",
-                "MED"
-        );
+        assertOppgaveFelter(oppgave);
+        assertThat(mockWebServer.takeRequest())
+                .extracting(RecordedRequest::getPath, RecordedRequest::getMethod)
+                .containsExactly("/oppgaver/" + OPPGAVE_ID, "GET");
     }
 
     @Test
-    void opprettOppgave_verifiserMapping() {
-        server.expect(requestTo("/oppgaver"))
-                .andExpect(method(HttpMethod.POST))
-                .andRespond(withSuccess().contentType(MediaType.APPLICATION_JSON).body(hentOppgaveResponse()));
-        var oppgaveDto = opprettOppgave();
-        var oppgave = oppgaveConsumer.opprettOppgave(oppgaveDto);
-
-        assertThat(oppgave)
-                .extracting(
-                        OppgaveDto::getJournalpostId,
-                        OppgaveDto::getAktoerId,
-                        OppgaveDto::getTema,
-                        OppgaveDto::getTildeltEnhetsnr
-                ).containsExactly(
-                oppgave.getJournalpostId(),
-                oppgave.getAktoerId(),
-                oppgave.getTema(),
-                oppgave.getTildeltEnhetsnr()
+    void opprettOppgave_verifiserMapping() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setBody(hentOppgaveResponse())
+                        .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
         );
+
+        var oppgave = oppgaveConsumer.opprettOppgave(opprettOppgave());
+        assertOppgaveFelter(oppgave);
+        assertThat(mockWebServer.takeRequest())
+                .extracting(RecordedRequest::getPath, RecordedRequest::getMethod)
+                .containsExactly("/oppgaver", "POST");
     }
 
     @Test
-    void oppdaterOppgave_utenBeskrivelse_beksrivelseMappesIkkeTilRequest() {
+    void oppdaterOppgave_utenBeskrivelse_beksrivelseMappesIkkeTilRequest() throws InterruptedException, JsonProcessingException {
         final var forventetJsonBodyRequestUtenBeskrivelseFelt = """
                 {
                     "id": 1,
@@ -83,13 +84,30 @@ class OppgaveConsumerTest {
                 }
                 """;
 
-        server.expect(requestTo("/oppgaver/" + OPPGAVE_ID))
-                .andExpect(method(HttpMethod.PATCH))
-                .andExpect(content().json(forventetJsonBodyRequestUtenBeskrivelseFelt))
-                .andRespond(withSuccess().contentType(MediaType.APPLICATION_JSON).body(hentOppgaveResponse()));
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setBody(hentOppgaveResponse())
+                        .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        );
 
         var oppgaveOppdateringDto = OppgaveOppdateringDto.builder().id(1).versjon(2).status("status").build();
-        assertThat(oppgaveConsumer.oppdaterOppgave(OPPGAVE_ID, oppgaveOppdateringDto)).isNotNull();
+        assertOppgaveFelter(oppgaveConsumer.oppdaterOppgave(OPPGAVE_ID, oppgaveOppdateringDto));
+
+        var request = mockWebServer.takeRequest();
+        var requestBody = request.getBody().readUtf8();
+        assertThat(request)
+                .extracting(RecordedRequest::getPath, RecordedRequest::getMethod)
+                .containsExactly("/oppgaver/" + OPPGAVE_ID, "PATCH");
+
+        var objectMapper = new ObjectMapper();
+        assertThat(objectMapper.readTree(requestBody))
+                .isEqualTo(objectMapper.readTree(forventetJsonBodyRequestUtenBeskrivelseFelt));
+    }
+
+    private void assertOppgaveFelter(HentOppgaveDto oppgaveDto) {
+        assertThat(oppgaveDto)
+                .extracting(HentOppgaveDto::getStatuskategori, HentOppgaveDto::getAktoerId, HentOppgaveDto::getTema)
+                .containsExactly("AAPEN", "1332607802528", "MED");
     }
 
     private OppgaveDto opprettOppgave() {
