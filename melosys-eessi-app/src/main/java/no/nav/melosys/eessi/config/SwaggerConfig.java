8393fe1c8fe6202d928f4dd060a6635e783691e3
Isa Salem Hame <wanghattan@gmail.com>
2022-04-05 10:16:16 +0200

    Bump Spring Boot 5.4.1 -> 5.6.6 (#449)
    * Bump Spring Boot 5.4.1 -> 5.6.6
    * Bump SpringFox-Swagger2 2.92 -> 3.0.0

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/config/SwaggerConfig.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/config/SwaggerConfig.java
@@ -1,18 +1,34 @@
 package no.nav.melosys.eessi.config;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import javax.servlet.ServletContext;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.RequestHandlerProvider;
 import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.WebMvcRequestHandler;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.DocumentationPluginsBootstrapper;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import springfox.documentation.spring.web.readers.operation.HandlerMethodResolver;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static java.util.stream.Collectors.toList;
+import static springfox.documentation.spi.service.contexts.Orderings.byPatternsCondition;
+import static springfox.documentation.spring.web.paths.Paths.ROOT;
 
 @Configuration
 @EnableSwagger2
@@ -21,21 +37,44 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(Konstanter.CONTROLLER_PAKKE))
-                .paths(PathSelectors.any())
-                .build()
-                .securityContexts(Collections.singletonList(
-                        SecurityContext.builder()
-                                .securityReferences(
-                                        Collections.singletonList(SecurityReference.builder()
-                                                .reference("JWT")
-                                                .scopes(new AuthorizationScope[0])
-                                                .build()
-                                        )
-                                )
-                                .build())
-                )
-                .securitySchemes(Collections.singletonList(new ApiKey("JWT", "Authorization", "header")));
+            .select()
+            .apis(RequestHandlerSelectors.basePackage(Konstanter.CONTROLLER_PAKKE))
+            .paths(PathSelectors.any())
+            .build()
+            .securityContexts(Collections.singletonList(
+                SecurityContext.builder()
+                    .securityReferences(
+                        Collections.singletonList(SecurityReference.builder()
+                            .reference("JWT")
+                            .scopes(new AuthorizationScope[0])
+                            .build()
+                        )
+                    )
+                    .build())
+            )
+            .securitySchemes(Collections.singletonList(new ApiKey("JWT", "Authorization", "header")));
+    }
+
+    @Bean
+    public InitializingBean removeSpringfoxHandlerProvider(DocumentationPluginsBootstrapper bootstrapper) {
+        return () -> bootstrapper.getHandlerProviders().removeIf(WebMvcRequestHandlerProvider.class::isInstance);
+    }
+
+    @Bean
+    public RequestHandlerProvider customRequestHandlerProvider(Optional<ServletContext> servletContext, HandlerMethodResolver methodResolver, List<RequestMappingInfoHandlerMapping> handlerMappings) {
+        String contextPath = servletContext.map(ServletContext::getContextPath).orElse(ROOT);
+        return () -> handlerMappings.stream()
+            .filter(mapping -> !mapping.getClass().getSimpleName().equals("IntegrationRequestMappingHandlerMapping"))
+            .map(mapping -> mapping.getHandlerMethods().entrySet())
+            .flatMap(Set::stream)
+            .map(entry -> new WebMvcRequestHandler(contextPath, methodResolver, tweakInfo(entry.getKey()), entry.getValue()))
+            .sorted(byPatternsCondition())
+            .collect(toList());
+    }
+
+    RequestMappingInfo tweakInfo(RequestMappingInfo info) {
+        if (info.getPathPatternsCondition() == null) return info;
+        String[] patterns = info.getPathPatternsCondition().getPatternValues().toArray(String[]::new);
+        return info.mutate().options(new RequestMappingInfo.BuilderConfiguration()).paths(patterns).build();
     }
 }
