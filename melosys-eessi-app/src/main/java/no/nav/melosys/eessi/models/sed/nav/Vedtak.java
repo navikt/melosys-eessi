50a44031898dec88ba36effa58c01afec7d56182
Ivan Skodje <ivanskodje@protonmail.com>
2022-12-05 10:58:49 +0100

    Oppdatert kommentar på Vedtak felt som har ufine regler

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/sed/nav/Vedtak.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/sed/nav/Vedtak.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public abstract class Vedtak {
     private String datoforrigevedtak;
-    private String eropprinneligvedtak; // Pga. RINA regler: Kan bare sette "ja" eller null (default: nei)
-    private String erendringsvedtak; // Pga. RINA regler: Kan bare sette "nei" eller null (default: ja)
+    private String eropprinneligvedtak; // RINA regler: Kan bare sette "ja" eller null (default: null, som betyr nei)
+    private String erendringsvedtak; // RINA regler: Kan bare sette "nei" eller null (default: null, som betyr ja)
 }
