9b641a56ff2d6bfb85121937869cb430773e2d2a
RuneLind <rulind@gmail.com>
2023-01-09 18:47:29 +0100

    Forandre til "sedMottatt feilet"

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/kafka/consumers/SedMottattConsumer.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/kafka/consumers/SedMottattConsumer.java
@@ -52,7 +52,7 @@ public class SedMottattConsumer {
         } catch (Exception e) {
             sedMetrikker.sedMottattFeilet(sedHendelse.getSedType());
             String message = e.getMessage() != null ? e.getMessage() : e.getClass().getSimpleName();
-            log.error("sedMottatt av sed feilet: {}\nsedHendelse: {}", message, sedHendelse, e);
+            log.error("sedMottatt feilet: {}\nsedHendelse: {}", message, sedHendelse, e);
         } finally {
             remove(SED_ID);
             remove(CORRELATION_ID);
