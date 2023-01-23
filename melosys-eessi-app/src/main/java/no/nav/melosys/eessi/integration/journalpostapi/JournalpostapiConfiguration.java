feaa89fd58222ce894ec68765368df18f58de5a7
Eirik Bergande <eirik.bergande@nav.no>
2023-01-23 10:18:53 +0100

    Refaktorerer resttemplate ut av journalpostapiconsumer bean

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/integration/journalpostapi/JournalpostapiConfiguration.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/integration/journalpostapi/JournalpostapiConfiguration.java
@@ -10,36 +10,43 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
-public class JournalpostapiConsumerProducer {
+public class JournalpostapiConfiguration {
 
     private final String url;
     private static final int CONNECT_TIMEOUT_SECONDS = 60;
     private static final int READ_TIMEOUT_SECONDS = 60;
 
-    public JournalpostapiConsumerProducer(@Value("${melosys.integrations.journalpostapi-url}") String url) {
+    public JournalpostapiConfiguration(@Value("${melosys.integrations.journalpostapi-url}") String url) {
         this.url = url;
     }
 
     @Bean
-    public JournalpostapiConsumer journalpostapiConsumer(
+    public RestTemplate journalpostapiRestTemplate(
         RestTemplateBuilder restTemplateBuilder,
         SystemContextClientRequestInterceptor systemContextClientRequestInterceptor,
-        CorrelationIdOutgoingInterceptor correlationIdOutgoingInterceptor,
-        ObjectMapper objectMapper,
-        Unleash unleash) {
-        RestTemplate restTemplate = restTemplateBuilder
+        CorrelationIdOutgoingInterceptor correlationIdOutgoingInterceptor) {
+        return restTemplateBuilder
+            .requestFactory(SimpleClientHttpRequestFactory::new)
             .uriTemplateHandler(new DefaultUriBuilderFactory(url))
             .interceptors(systemContextClientRequestInterceptor, correlationIdOutgoingInterceptor)
             .setConnectTimeout(Duration.ofSeconds(CONNECT_TIMEOUT_SECONDS))
             .setReadTimeout(Duration.ofSeconds(READ_TIMEOUT_SECONDS))
             .setBufferRequestBody(false)
             .build();
+    }
 
-        return new JournalpostapiConsumer(restTemplate, objectMapper, unleash);
+    @Bean
+    public JournalpostapiConsumer journalpostapiConsumer(
+        RestTemplate journalpostapiRestTemplate,
+        ObjectMapper objectMapper,
+        Unleash unleash) {
+
+        return new JournalpostapiConsumer(journalpostapiRestTemplate, objectMapper, unleash);
     }
 
 }
