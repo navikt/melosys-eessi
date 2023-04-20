fe4e8a0819c0d5eb7a92bfb926547ec181e09cd9
Isa Salem Hame <wanghattan@gmail.com>
2023-04-20 11:28:09 +0200

    4570: Legger til tom konstruktør til DTOs for X008 fordi Jackson trenger det

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/sed/nav/InvalideringSed.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/sed/nav/InvalideringSed.java
@@ -2,9 +2,13 @@ package no.nav.melosys.eessi.models.sed.nav;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class InvalideringSed {
 
     private String utstedelsesdato;
--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/sed/nav/Ugyldiggjoere.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/sed/nav/Ugyldiggjoere.java
@@ -3,8 +3,10 @@ package no.nav.melosys.eessi.models.sed.nav;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Ugyldiggjoere {
