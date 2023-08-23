e53129edf9881666e9ece1ce7ab07ef4a98b0f45
Isa Salem Hame <wanghattan@gmail.com>
2023-08-23 12:28:49 +0200

    Hotfix: Registrer java time module i josn parsing

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/kafkadlq/OppgaveHendelseAivenKafkaDLQ.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/kafkadlq/OppgaveHendelseAivenKafkaDLQ.java
@@ -2,6 +2,7 @@ package no.nav.melosys.eessi.models.kafkadlq;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,6 +27,7 @@ public class OppgaveHendelseAivenKafkaDLQ extends KafkaDLQ {
     @Override
     public String hentMeldingSomStreng() throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         return objectMapper.writeValueAsString(oppgaveEndretHendelse);
     }
 }
