cd188c0bfaa948289dd5be88410ccc9bcd96fe5d
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2024-02-19 13:22:31 +0100

    Fjernet debug log

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/metrikker/SedMetrikker.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/metrikker/SedMetrikker.java
@@ -62,8 +62,6 @@ public class SedMetrikker {
     }
 
     public void sedPdfKonverteringFeilet() {
-        // TODO: Fjern log
-        log.info("Inkrementerer " + SED_KONVERTERING_FEILET + "til " + (Metrics.counter(SED_KONVERTERING_FEILET).count() + 1));
         Metrics.counter(SED_KONVERTERING_FEILET).increment();
     }
 }
