4f31d27a94ad729e727492750aed1b489300ac19
Janita Johansen <janita.marielle.johansen@nav.no>
2024-02-22 15:25:54 +0100

    Prøver noe

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/sed/mapper/til_sed/lovvalg/LovvalgSedMapper.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/sed/mapper/til_sed/lovvalg/LovvalgSedMapper.java
@@ -23,7 +23,7 @@ public interface LovvalgSedMapper<T extends Medlemskap> extends SedMapper {
     default void setVedtaksdata(Vedtak vedtak, VedtakDto vedtakDto) {
         if (vedtakDto != null && !vedtakDto.isErFørstegangsvedtak()) {
             vedtak.setEropprinneligvedtak(null);
-            vedtak.setErendringsvedtak(null);
+            vedtak.setErendringsvedtak("nei");
             vedtak.setDatoforrigevedtak(
                 vedtakDto.getDatoForrigeVedtak() != null ? vedtakDto.getDatoForrigeVedtak().toString() : null
             );
