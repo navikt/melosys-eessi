759d2f73087b8a1e8144faf39912a3cea3b19976
Øystein Barstad <111733896+barstadEkstern@users.noreply.github.com>
2024-04-23 15:15:17 +0200

    6530 Sjekk ident for jwt token (#650)
    https://jira.adeo.no/browse/MELOSYS-6530
    Utvider canfetchOBOtoken til å sjekke om ident faktisk eksisterer i
    token.

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/security/ContextHolder.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/security/ContextHolder.java
@@ -1,8 +1,15 @@
 package no.nav.melosys.eessi.security;
 
+import com.nimbusds.jwt.SignedJWT;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.security.token.support.core.context.TokenValidationContext;
+import no.nav.security.token.support.core.jwt.JwtToken;
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder;
 
+import java.text.ParseException;
+import java.util.Map;
+
+@Slf4j
 final class ContextHolder {
 
     private final SpringTokenValidationContextHolder context;
@@ -10,6 +17,8 @@ final class ContextHolder {
     private static ContextHolder instans;
 
     private static final String AAD = "aad";
+    private static final String IDTYP = "idtyp";
+    private static final String APP = "app";
 
     private ContextHolder(SpringTokenValidationContextHolder context) {
         this.context = context;
@@ -22,8 +31,20 @@ final class ContextHolder {
         return instans;
     }
 
-    boolean canExchangeOBOToken() {
-        return getTokenContext().getJwtToken(AAD) != null;
+    boolean canExchangeOBOToken() throws ParseException {
+        JwtToken jwtToken = getTokenContext().getJwtToken(AAD);
+        try {
+            return (jwtToken != null && !aktørErApplikasjon(jwtToken));
+        } catch (ParseException e) {
+            log.error("Feil ved parsing av JWT token", e);
+            throw e;
+        }
+    }
+
+    boolean aktørErApplikasjon(JwtToken jwtToken) throws ParseException {
+        SignedJWT jwt = SignedJWT.parse(jwtToken.getTokenAsString());
+        Map<String, Object> payload = jwt.getPayload().toJSONObject();
+        return payload.containsKey(IDTYP) && payload.get(IDTYP).equals(APP);
     }
 
     private TokenValidationContext getTokenContext() {
