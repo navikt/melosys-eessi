90802072126e180dcf7c35d2d66d9a3130a1ef2e
Isa Salem Hame <wanghattan@gmail.com>
2024-05-06 12:35:31 +0200

    Bruk  GenericAuthExchangeFilter for oppgave

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/integration/oppgave/OppgaveConsumerProducer.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/integration/oppgave/OppgaveConsumerProducer.java
@@ -2,10 +2,11 @@ package no.nav.melosys.eessi.integration.oppgave;
 
 import java.util.Collections;
 
-import no.nav.melosys.eessi.security.OppgaveSakRequestExchangeFilter;
+import no.nav.melosys.eessi.security.GenericAuthFilterFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -20,13 +21,15 @@ public class OppgaveConsumerProducer {
     }
 
     @Bean
-    public OppgaveConsumer oppgaveConsumer(WebClient.Builder webClientBuilder, OppgaveSakRequestExchangeFilter oppgaveSakRequestExchangeFilter) {
+    @Primary
+    public OppgaveConsumer oppgaveConsumer(WebClient.Builder webClientBuilder,
+                                           GenericAuthFilterFactory genericAuthFilterFactory) {
         return new OppgaveConsumer(
-                webClientBuilder
-                        .baseUrl(url)
-                        .defaultHeaders(this::defaultHeaders)
-                        .filter(oppgaveSakRequestExchangeFilter)
-                        .build()
+            webClientBuilder
+                .baseUrl(url)
+                .defaultHeaders(this::defaultHeaders)
+                .filter(genericAuthFilterFactory.getAzureFilter("oppgave-sak"))
+                .build()
         );
     }
 
