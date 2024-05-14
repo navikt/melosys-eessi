6527a015b222c5658c3c62c4474a6b73f2a133cd
Thang Phan <thang.quoc.ly.phan@nav.no>
2024-05-14 11:16:56 +0200

    WIP

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/eux/EuxTokenContextService.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/eux/EuxTokenContextService.java
@@ -1,6 +1,7 @@
 package no.nav.melosys.eessi.service.eux;
 
 import no.nav.melosys.eessi.integration.eux.rina_api.EuxConsumer;
+import no.nav.melosys.eessi.integration.eux.rina_api.EuxRinasakerConsumer;
 import no.nav.melosys.eessi.metrikker.BucMetrikker;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,8 @@ import org.springframework.stereotype.Component;
 public class EuxTokenContextService extends EuxService {
 
     public EuxTokenContextService(@Qualifier("tokenContext") EuxConsumer euxConsumer,
+                                  @Qualifier("tokenContext") EuxRinasakerConsumer euxRinasakerConsumer,
                                   BucMetrikker bucMetrikker) {
-        super(euxConsumer, bucMetrikker);
+        super(euxConsumer, bucMetrikker, euxRinasakerConsumer);
     }
 }
