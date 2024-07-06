4157a947d57a7605a4f19c276f11478421190dcf
RuneLind <rulind@gmail.com>
2024-07-06 23:23:59 +0200

    Bruk sed.Konstanter

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/buc/SedVersjonSjekker.kt
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/buc/SedVersjonSjekker.kt
@@ -3,15 +3,15 @@ package no.nav.melosys.eessi.models.buc
 import mu.KotlinLogging
 import no.nav.melosys.eessi.models.sed.SED
 import java.util.regex.Pattern
+import no.nav.melosys.eessi.models.sed.Konstanter.DEFAULT_SED_G_VER
+import no.nav.melosys.eessi.models.sed.Konstanter.DEFAULT_SED_VER
 
 private val log = KotlinLogging.logger {}
 
 object SedVersjonSjekker {
     private val VERSJON_PATTERN = Pattern.compile("^v(\\d)\\.(\\d)$")
-    private const val DEFAULT_SED_G_VER = "1"  // Assuming default value
-    private const val DEFAULT_SED_VER = "0"  // Assuming default value
 
-    @JvmStatic // TODO: fjent når filer som bruker er konvertert til Kotlin
+    @JvmStatic // TODO: fjern når filer som bruker er konvertert til Kotlin
     fun verifiserSedVersjonErBucVersjon(buc: BUC, sed: SED) {
         val ønsketSedVersjon = "v${sed.sedGVer}.${sed.sedVer}"
         if (!ønsketSedVersjon.equals(buc.bucVersjon, ignoreCase = true)) {
@@ -21,13 +21,13 @@ object SedVersjonSjekker {
         }
     }
 
-    @JvmStatic // TODO: fjent når filer som bruker er konvertert til Kotlin
+    @JvmStatic // TODO: fjern når filer som bruker er konvertert til Kotlin
     fun parseGVer(buc: BUC): String {
         val matcher = VERSJON_PATTERN.matcher(buc.bucVersjon!!)
         return if (matcher.find()) matcher.group(1) else DEFAULT_SED_G_VER
     }
 
-    @JvmStatic // TODO: fjent når filer som bruker er konvertert til Kotlin
+    @JvmStatic // TODO: fjern når filer som bruker er konvertert til Kotlin
     fun parseVer(buc: BUC): String {
         val matcher = VERSJON_PATTERN.matcher(buc.bucVersjon!!)
         return if (matcher.find()) matcher.group(2) else DEFAULT_SED_VER
