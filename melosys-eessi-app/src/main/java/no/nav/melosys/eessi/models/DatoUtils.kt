876246325c1202b8484796822d329752db42bcda
RuneLind <rulind@gmail.com>
2024-07-09 13:05:12 +0200

    Rydd i DatoUtils

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/DatoUtils.kt
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/DatoUtils.kt
@@ -3,30 +3,17 @@ package no.nav.melosys.eessi.models
 import java.time.LocalDate
 import javax.xml.datatype.XMLGregorianCalendar
 
-class DatoUtils private constructor() {
-    init {
-        throw UnsupportedOperationException("This is a utility class and cannot be instantiated")
-    }
+object DatoUtils {
+    private const val LOCAL_DATE_LENGTH = 10
 
-    companion object {
-        private const val LOCAL_DATE_LENGTH = 10
+    @JvmStatic
+    fun tilLocalDate(dato: String): LocalDate = LocalDate.parse(
+        dato.substring(0, LOCAL_DATE_LENGTH)
+    )
 
-        @JvmStatic
-        fun tilLocalDate(dato: String): LocalDate {
-            return LocalDate.parse(formaterDatoString(dato))
+    @JvmStatic
+    fun tilLocalDate(xmlGregorianCalendar: XMLGregorianCalendar?): LocalDate? =
+        xmlGregorianCalendar?.let {
+            LocalDate.of(it.year, it.month, it.day)
         }
-
-        private fun formaterDatoString(dato: String): String {
-            return dato.substring(0, LOCAL_DATE_LENGTH)
-        }
-
-        @JvmStatic
-        fun tilLocalDate(xmlGregorianCalendar: XMLGregorianCalendar?): LocalDate? {
-            return if (xmlGregorianCalendar != null) LocalDate.of(
-                xmlGregorianCalendar.year,
-                xmlGregorianCalendar.month,
-                xmlGregorianCalendar.day
-            ) else null
-        }
-    }
 }
