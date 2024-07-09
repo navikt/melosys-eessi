6bf962dbcd755f98f840a41dfbaafcb682582653
RuneLind <rulind@gmail.com>
2024-07-09 20:43:13 +0200

    Bruk non-nullable og Rydd i BucIdentifiseringOppg

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/BucIdentifiseringOppg.kt
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/BucIdentifiseringOppg.kt
@@ -10,38 +10,35 @@ class BucIdentifiseringOppg(
     var id: Long = 0,
 
     @Column(name = "rina_saksnummer", nullable = false)
-    var rinaSaksnummer: String? = null,
+    var rinaSaksnummer: String,
 
     @Column(name = "oppgave_id", nullable = false)
-    var oppgaveId: String? = null,
+    var oppgaveId: String,
 
     @Column(name = "versjon")
     var versjon: Int = 0
 ) {
+    override fun toString(): String =
+        "BucIdentifiseringOppg(id=$id, rinaSaksnummer='$rinaSaksnummer', oppgaveId='$oppgaveId', versjon=$versjon)"
+
     // TODO: fjern builder når vi har ting i kotlin
     class BucIdentifiseringOppgBuilder {
-        private var id: Long? = null
         private var rinaSaksnummer: String? = null
         private var oppgaveId: String? = null
         private var versjon: Int = 0
 
-        fun id(id: Long?) = apply { this.id = id }
         fun rinaSaksnummer(rinaSaksnummer: String?) = apply { this.rinaSaksnummer = rinaSaksnummer }
         fun oppgaveId(oppgaveId: String?) = apply { this.oppgaveId = oppgaveId }
         fun versjon(versjon: Int) = apply { this.versjon = versjon }
 
-        fun build(): BucIdentifiseringOppg {
-            return BucIdentifiseringOppg(
-                id ?: 0,
-                rinaSaksnummer ?: throw IllegalArgumentException("rinaSaksnummer kan ikke være null"),
-                oppgaveId ?: throw IllegalArgumentException("oppgaveId kan ikke være null"),
-                versjon
-            )
-        }
+        fun build() = BucIdentifiseringOppg(
+            rinaSaksnummer = rinaSaksnummer ?: throw IllegalArgumentException("rinaSaksnummer kan ikke være null"),
+            oppgaveId = oppgaveId ?: throw IllegalArgumentException("oppgaveId kan ikke være null"),
+            versjon = versjon
+        )
 
-        override fun toString(): String {
-            return "BucIdentifiseringOppgBuilder(id=$id, rinaSaksnummer=$rinaSaksnummer, oppgaveId=$oppgaveId, versjon=$versjon)"
-        }
+        override fun toString(): String =
+            "BucIdentifiseringOppgBuilder(rinaSaksnummer=$rinaSaksnummer, oppgaveId=$oppgaveId, versjon=$versjon)"
     }
 
     companion object {
