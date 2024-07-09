d4c273180da4db146c137ba09eec9f07b0ac69e2
RuneLind <rulind@gmail.com>
2024-07-09 13:07:39 +0200

    models DatoUtilsTest -> kotlin

--- melosys-eessi-app/src/test/java/no/nav/melosys/eessi/models/DatoUtilsTest.kt
+++ melosys-eessi-app/src/test/java/no/nav/melosys/eessi/models/DatoUtilsTest.kt
@@ -1,19 +1,17 @@
-package no.nav.melosys.eessi.models;
+package no.nav.melosys.eessi.models
 
-import java.time.LocalDate;
-
-import org.junit.jupiter.api.Test;
-
-import static no.nav.melosys.eessi.models.DatoUtils.tilLocalDate;
-import static org.assertj.core.api.Assertions.assertThat;
+import io.kotest.matchers.shouldBe
+import no.nav.melosys.eessi.models.DatoUtils.tilLocalDate
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 class DatoUtilsTest {
 
     @Test
-    void tilLocalDate_stringMedTime_parserTilLocaldate() {
-        final String input = "2020-01-01:+02:00";
-        final LocalDate forventet = LocalDate.of(2020, 1, 1);
-        assertThat(tilLocalDate(input)).isEqualTo(forventet);
+    fun tilLocalDate_stringMedTime_parserTilLocaldate() {
+        val input = "2020-01-01:+02:00"
+        val forventet = LocalDate.of(2020, 1, 1)
 
+        tilLocalDate(input) shouldBe forventet
     }
 }
