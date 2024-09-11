1f383ee49e2ee89dc671f083a7057a584cadd0c3
RuneLind <rulind@gmail.com>
2024-09-11 14:46:05 +0200

    Rydd i JournalpostSedKoblingService kotlin konvertering

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/journalpostkobling/JournalpostSedKoblingService.kt
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/journalpostkobling/JournalpostSedKoblingService.kt
@@ -1,20 +1,21 @@
 package no.nav.melosys.eessi.service.journalpostkobling
 
+import mu.KotlinLogging
 import no.nav.melosys.eessi.integration.saf.SafConsumer
 import no.nav.melosys.eessi.kafka.producers.model.MelosysEessiMelding
 import no.nav.melosys.eessi.models.FagsakRinasakKobling
 import no.nav.melosys.eessi.models.JournalpostSedKobling
 import no.nav.melosys.eessi.models.SedType
-import no.nav.melosys.eessi.models.buc.Document
 import no.nav.melosys.eessi.models.sed.SED
 import no.nav.melosys.eessi.repository.JournalpostSedKoblingRepository
 import no.nav.melosys.eessi.service.eux.EuxService
 import no.nav.melosys.eessi.service.saksrelasjon.SaksrelasjonService
 import no.nav.melosys.eessi.service.sed.mapper.fra_sed.melosys_eessi_melding.MelosysEessiMeldingMapperFactory
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.*
+import kotlin.jvm.optionals.getOrNull
+
+private val log = KotlinLogging.logger {}
 
 @Service
 class JournalpostSedKoblingService(
@@ -24,84 +25,21 @@ class JournalpostSedKoblingService(
     private val safConsumer: SafConsumer,
     private val melosysEessiMeldingMapperFactory: MelosysEessiMeldingMapperFactory
 ) {
-    fun finnVedJournalpostID(journalpostID: String): Optional<JournalpostSedKobling> {
-        return journalpostSedKoblingRepository.findByJournalpostID(journalpostID)
-    }
+    fun finnVedJournalpostID(journalpostID: String): Optional<JournalpostSedKobling> =
+        journalpostSedKoblingRepository.findByJournalpostID(journalpostID)
 
     fun finnVedJournalpostIDOpprettMelosysEessiMelding(journalpostID: String): Optional<MelosysEessiMelding> {
-        val journalpostSedKobling = journalpostSedKoblingRepository.findByJournalpostID(journalpostID)
-        if (journalpostSedKobling.isPresent) {
-            return Optional.of(opprettEessiMelding(journalpostSedKobling.get()))
-        }
-        val rinaSaksnummer = søkEtterRinaSaksnummerForJournalpost(journalpostID)
-        if (rinaSaksnummer.isPresent) {
-            return opprettEessiMelding(rinaSaksnummer.get(), journalpostID)
-        }
-        return Optional.empty()
-    }
-
-    fun erASedAlleredeBehandlet(rinaSaksnummer: String): Boolean {
-        return journalpostSedKoblingRepository.findByRinaSaksnummer(rinaSaksnummer).stream().anyMatch { obj: JournalpostSedKobling -> obj.erASed() }
-    }
-
-    private fun søkEtterRinaSaksnummerForJournalpost(journalpostID: String): Optional<String> {
-        val rinaSaksnummer = safConsumer.hentRinasakForJournalpost(journalpostID)
-        if (rinaSaksnummer.isEmpty) {
-            log.info("Rinasaksnummer er null fra saf for journalpostId: {}", journalpostID)
-        }
-        return rinaSaksnummer
-    }
-
-    private fun opprettEessiMelding(journalpostSedKobling: JournalpostSedKobling): MelosysEessiMelding {
-        val buc = euxService.hentBuc(journalpostSedKobling.rinaSaksnummer)
-        val organisation = buc.hentDokument(journalpostSedKobling.sedId).creator!!.organisation
-        val sed = euxService.hentSed(journalpostSedKobling.rinaSaksnummer, journalpostSedKobling.sedId)
-        val gsakSaksnummer =
-            saksrelasjonService.finnVedRinaSaksnummer(journalpostSedKobling.rinaSaksnummer).map(FagsakRinasakKobling::gsakSaksnummer).orElse(null)
-        return opprettMelosysEessiMelding(
-            sed!!,
-            journalpostSedKobling.sedId,
-            journalpostSedKobling.rinaSaksnummer,
-            journalpostSedKobling.sedType,
-            journalpostSedKobling.bucType,
-            organisation!!.id,
-            organisation.countryCode,
-            journalpostSedKobling.journalpostID,
-            gsakSaksnummer?.toString(),
-            journalpostSedKobling.sedVersjon.toInt() != 1,
-            journalpostSedKobling.sedVersjon
+        val journalpostSedKobling = journalpostSedKoblingRepository.findByJournalpostID(journalpostID).getOrNull()
+        return Optional.ofNullable(
+            journalpostSedKobling?.let { opprettEessiMelding(it) }
+                ?: søkEtterRinaSaksnummerForJournalpost(journalpostID)?.let { rinaSaksnummer ->
+                    opprettEessiMelding(rinaSaksnummer, journalpostID)
+                }
         )
     }
 
-    private fun opprettEessiMelding(rinaSaksnummer: String, journalpostID: String): Optional<MelosysEessiMelding> {
-        val buc = euxService.hentBuc(rinaSaksnummer)
-        val documentOptional = buc.hentSistOppdaterteDocument()
-        if (documentOptional == null) {
-            log.warn("Finner ikke sist oppdaterte sed for rinasak {}", rinaSaksnummer)
-            return Optional.empty()
-        }
-        val sedDocument: Document = documentOptional
-        val sedID = sedDocument.id
-        val sedType = sedDocument.type
-        val organisation = sedDocument.creator!!.organisation
-        val sed = euxService.hentSed(rinaSaksnummer, sedID)
-        val sedVersjon = "0" //har ikke sed-versjon
-        return Optional.of(
-            opprettMelosysEessiMelding(
-                sed!!,
-                sedID,
-                rinaSaksnummer,
-                sedType,
-                buc.bucType,
-                organisation!!.id,
-                organisation.countryCode,
-                journalpostID,
-                null,
-                false,
-                sedVersjon
-            )
-        )
-    }
+    fun erASedAlleredeBehandlet(rinaSaksnummer: String): Boolean =
+        journalpostSedKoblingRepository.findByRinaSaksnummer(rinaSaksnummer).any { it.erASed() }
 
     fun lagre(
         journalpostID: String,
@@ -110,8 +48,64 @@ class JournalpostSedKoblingService(
         sedVersjon: String,
         bucType: String,
         sedType: String
-    ): JournalpostSedKobling {
-        return journalpostSedKoblingRepository.save(JournalpostSedKobling(journalpostID, rinaSaksnummer, sedID, sedVersjon, bucType, sedType))
+    ): JournalpostSedKobling =
+        journalpostSedKoblingRepository.save(JournalpostSedKobling(journalpostID, rinaSaksnummer, sedID, sedVersjon, bucType, sedType))
+
+    private fun søkEtterRinaSaksnummerForJournalpost(journalpostID: String): String? =
+        safConsumer.hentRinasakForJournalpost(journalpostID).getOrNull()?.also {
+            log.info("Rinasaksnummer er null fra saf for journalpostId: {}", journalpostID)
+        }
+
+    private fun opprettEessiMelding(journalpostSedKobling: JournalpostSedKobling): MelosysEessiMelding {
+        val buc = euxService.hentBuc(journalpostSedKobling.rinaSaksnummer)
+        val organisation = buc.hentDokument(journalpostSedKobling.sedId).creator!!.organisation
+        val sed = euxService.hentSed(journalpostSedKobling.rinaSaksnummer, journalpostSedKobling.sedId)
+        val gsakSaksnummer = finnVedRinaSaksnummer(journalpostSedKobling)?.gsakSaksnummer
+        return opprettMelosysEessiMelding(
+            sed = sed!!,
+            sedId = journalpostSedKobling.sedId,
+            rinaSaksnummer = journalpostSedKobling.rinaSaksnummer,
+            sedType = journalpostSedKobling.sedType,
+            bucType = journalpostSedKobling.bucType,
+            avsenderID = organisation!!.id,
+            landkode = organisation.countryCode,
+            journalpostID = journalpostSedKobling.journalpostID,
+            saksnummer = gsakSaksnummer?.toString(),
+            erEndring = journalpostSedKobling.sedVersjon.toInt() != 1,
+            sedVersjon = journalpostSedKobling.sedVersjon
+        ) ?: throw IllegalStateException("Kunne ikke opprette melding for journalpostSedKobling: $journalpostSedKobling")
+    }
+
+    private fun finnVedRinaSaksnummer(journalpostSedKobling: JournalpostSedKobling): FagsakRinasakKobling? =
+        saksrelasjonService.finnVedRinaSaksnummer(journalpostSedKobling.rinaSaksnummer).getOrNull()
+
+    private fun opprettEessiMelding(rinaSaksnummer: String, journalpostID: String): MelosysEessiMelding? {
+        val buc = euxService.hentBuc(rinaSaksnummer)
+        val documentOptional = buc.hentSistOppdaterteDocument() ?: run {
+            log.warn("Finner ikke sist oppdaterte sed for rinasak {}", rinaSaksnummer)
+            return null
+        }
+        val sedDocument = documentOptional
+        val sedID = sedDocument.id
+        val sedType = sedDocument.type
+        val organisation = sedDocument.creator!!.organisation
+        val sed = euxService.hentSed(rinaSaksnummer, sedID)
+        val sedVersjon = "0" // har ikke sed-versjon
+        return sed?.let {
+            opprettMelosysEessiMelding(
+                sed = sed,
+                sedId = sedID,
+                rinaSaksnummer = rinaSaksnummer,
+                sedType = sedType,
+                bucType = buc.bucType,
+                avsenderID = organisation!!.id,
+                landkode = organisation.countryCode,
+                journalpostID = journalpostID,
+                saksnummer = null,
+                erEndring = false,
+                sedVersjon = sedVersjon
+            )
+        } ?: throw IllegalStateException("Sed er null - Kunne ikke opprette melding for rinasak: $rinaSaksnummer")
     }
 
     private fun opprettMelosysEessiMelding(
@@ -126,12 +120,8 @@ class JournalpostSedKoblingService(
         saksnummer: String?,
         erEndring: Boolean,
         sedVersjon: String
-    ): MelosysEessiMelding {
+    ): MelosysEessiMelding? {
         return melosysEessiMeldingMapperFactory.getMapper(SedType.valueOf(sedType!!))
             .map(null, sed, sedId, rinaSaksnummer, sedType, bucType, avsenderID, landkode, journalpostID, null, saksnummer, erEndring, sedVersjon)
-    }
-
-    companion object {
-        private val log: Logger = LoggerFactory.getLogger(JournalpostSedKoblingService::class.java)
     }
 }
