acbf11740cc414bb5dda72d51795512980ed8c51
RuneLind <rulind@gmail.com>
2024-10-11 14:39:22 +0200

    Legg på TODO for fiksing i egen pr.

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/eux/OpprettBucOgSedResponse.kt
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/eux/OpprettBucOgSedResponse.kt
@@ -1,6 +1,6 @@
 package no.nav.melosys.eessi.service.eux
 
 data class OpprettBucOgSedResponse(
-    val rinaSaksnummer: String? = null,
-    val dokumentId: String? = null
+    val rinaSaksnummer: String? = null, // TODO: kan ikke være null, tar i egen pr.
+    val dokumentId: String? = null // TODO: kan ikke være null, tar i egen pr.
 )
