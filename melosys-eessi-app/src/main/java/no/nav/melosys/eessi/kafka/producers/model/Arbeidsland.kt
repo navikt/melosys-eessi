27cb9be41348345613476f769bdbd1e30a2423ab
Rune Lind <rune.lind@nav.no>
2024-10-17 09:44:25 +0200

    6523 Kosovo landkode (#693)
    Legg til landkode for Kosovo i LandkodeMapper
    Og tillatt nullable fra tidligere Kotlin konvertering

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/kafka/producers/model/Arbeidsland.kt
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/kafka/producers/model/Arbeidsland.kt
@@ -2,10 +2,10 @@ package no.nav.melosys.eessi.kafka.producers.model
 
 data class Arbeidsland(
     var land: String?,
-    var arbeidssted: List<Arbeidssted>
+    var arbeidssted: List<Arbeidssted>?
 ) {
     constructor(arbeidsland: no.nav.melosys.eessi.models.sed.nav.Arbeidsland) : this(
         arbeidsland.land,
-        arbeidsland.arbeidssted!!.map { Arbeidssted(it) }
+        arbeidsland.arbeidssted?.map { Arbeidssted(it) }
     )
 }
