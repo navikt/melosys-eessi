6782db75cefc25b35b936d88098545006cbe9691
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-02-06 14:30:37 +0100

    7121 fjern arbeidsland fra a002 (#717)
    https://jira.adeo.no/browse/MELOSYS-7121

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/sed/mapper/til_sed/SedMapper.kt
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/service/sed/mapper/til_sed/SedMapper.kt
@@ -40,13 +40,13 @@ interface SedMapper {
 
     fun prefillNav(sedData: SedDataDto): Nav {
         val erSedMatch = getSedType() in setOf(
-            SedType.A001, SedType.A002, SedType.A003,
+            SedType.A001, SedType.A003,
             SedType.A008, SedType.A009, SedType.A010
         )
         return Nav(
             arbeidssted = null,
             arbeidsland = if (erSedMatch) hentArbeidsland(sedData).takeIf { it.isNotEmpty() } else null,
-            harfastarbeidssted = if (sedData.harFastArbeidssted == true) "ja" else "nei",
+            harfastarbeidssted = if (!erSedMatch) null else if (sedData.harFastArbeidssted == true) "ja" else "nei",
             bruker = hentBruker(sedData),
             arbeidsgiver = hentArbeidsgivereILand(
                 sedData.arbeidsgivendeVirksomheter,
@@ -71,7 +71,11 @@ interface SedMapper {
             }.apply {
                 if (isEmpty()) {
                     throw MappingException(
-                        "Statsborgerskap mangler eller er ugyldig. Statsborgerskap fra sedData: ${sedDataDto.bruker.statsborgerskap.joinToString(", ")}"
+                        "Statsborgerskap mangler eller er ugyldig. Statsborgerskap fra sedData: ${
+                            sedDataDto.bruker.statsborgerskap.joinToString(
+                                ", "
+                            )
+                        }"
                     )
                 }
             }
@@ -214,7 +218,10 @@ interface SedMapper {
             )
         }
 
-    private fun hentArbeidsgiver(virksomheter: List<Virksomhet>, virksomhetPredicate: (Virksomhet) -> Boolean): List<Arbeidsgiver> =
+    private fun hentArbeidsgiver(
+        virksomheter: List<Virksomhet>,
+        virksomhetPredicate: (Virksomhet) -> Boolean
+    ): List<Arbeidsgiver> =
         virksomheter
             .filter(virksomhetPredicate)
             .map { hentArbeidsgiver(it) }
@@ -266,7 +273,8 @@ interface SedMapper {
 
     private fun formaterDato(dato: LocalDate): String = Konstanter.dateTimeFormatter.format(dato)
 
-    fun LocalDate?.formater(): String = this?.let { formaterDato(it) } ?: throw MappingException("dato kan ikke være null")
+    fun LocalDate?.formater(): String =
+        this?.let { formaterDato(it) } ?: throw MappingException("dato kan ikke være null")
 
     fun LocalDate?.formaterEllerNull(): String? = this?.let { formaterDato(it) }
 }
