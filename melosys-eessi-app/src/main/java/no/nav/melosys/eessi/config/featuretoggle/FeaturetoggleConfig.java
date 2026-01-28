45d3f525b9580ed0a5d47f4aca6447528671d82b
oyvinyg <oyvinyg@gmail.com>
2026-01-28 13:47:18 +0100

    Bruke separate beans for unleash i forskjellige profiles

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/config/featuretoggle/FeaturetoggleConfig.java
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/config/featuretoggle/FeaturetoggleConfig.java
@@ -1,40 +1,53 @@
 package no.nav.melosys.eessi.config.featuretoggle;
 
-import java.util.Collections;
-import java.util.List;
-
 import io.getunleash.DefaultUnleash;
 import io.getunleash.FakeUnleash;
 import io.getunleash.Unleash;
 import io.getunleash.util.UnleashConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Profile;
 
+@Slf4j
 @Configuration
 public class FeaturetoggleConfig {
 
     private static final String APP_NAME = "Melosys-eessi";
 
-    @Value("${unleash.url}")
-    private String unleashUrl;
+    @Bean
+    @Profile({"nais", "local-mock"})
+    public UnleashConfig unleashConfig(@Value("${unleash.url}") String url,
+                                        @Value("${unleash.token}") String token) {
+        log.info("Configuring Unleash with URL: {}", url);
+        return UnleashConfig.builder()
+            .apiKey(token)
+            .appName(APP_NAME)
+            .unleashAPI(url)
+            .build();
+    }
 
     @Bean
-    public Unleash unleash(Environment environment, @Value("${unleash.token}") String token) {
+    @Profile("nais")
+    public Unleash unleash(UnleashConfig config) {
+        log.info("Creating DefaultUnleash for nais profile");
+        return new DefaultUnleash(config);
+    }
 
-        if (!Collections.disjoint(List.of(environment.getActiveProfiles()), List.of("local", "local-mock", "local-q2", "test"))) {
-            var fakeUnleash = new FakeUnleash();
-            fakeUnleash.enableAll();
-            return fakeUnleash;
-        } else {
-            var unleashConfig = UnleashConfig.builder()
-                .apiKey(token)
-                .appName(APP_NAME)
-                .unleashAPI(unleashUrl)
-                .build();
+    @Bean
+    @Profile("local-mock")
+    public Unleash localUnleash(UnleashConfig config) {
+        log.info("Creating DefaultEnabledUnleash for local-mock profile");
+        return new DefaultEnabledUnleash(new DefaultUnleash(config));
+    }
 
-            return new DefaultUnleash(unleashConfig);
-        }
+    @Bean
+    @Profile({"local", "local-q2", "test"})
+    public Unleash fakeUnleash() {
+        log.info("Creating FakeUnleash with all toggles enabled");
+        var fakeUnleash = new FakeUnleash();
+        fakeUnleash.enableAll();
+        return fakeUnleash;
     }
 }
