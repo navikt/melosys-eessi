36e3e86addb26b9f9033c75db14aef7a5720e03e
RuneLind <rulind@gmail.com>
2026-02-06 17:51:22 +0100

    7553 A008: Logg advarsel ved flerverdi arbeidiflereland-array
    Legger til log.warn i ArbeidIFlereLandDeserializer naar
    innkommende arbeidiflereland-array har mer enn ett element,
    siden kun foerste element brukes ved deserialisering.
    
    Uten advarsel forkastes ekstra elementer stille, noe som
    kan skjule datatap dersom RINA sender flere enn ett element.

--- melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/sed/ArbeidIFlereLandDeserializer.kt
+++ melosys-eessi-app/src/main/java/no/nav/melosys/eessi/models/sed/ArbeidIFlereLandDeserializer.kt
@@ -1,16 +1,22 @@
 package no.nav.melosys.eessi.models.sed
 
 import no.nav.melosys.eessi.models.sed.nav.ArbeidIFlereLand
+import org.slf4j.LoggerFactory
 import tools.jackson.core.JsonParser
 import tools.jackson.databind.DeserializationContext
 import tools.jackson.databind.deser.std.StdDeserializer
 
 class ArbeidIFlereLandDeserializer : StdDeserializer<ArbeidIFlereLand>(ArbeidIFlereLand::class.java) {
 
+    private val log = LoggerFactory.getLogger(ArbeidIFlereLandDeserializer::class.java)
+
     override fun deserialize(jsonParser: JsonParser, deserializationContext: DeserializationContext): ArbeidIFlereLand {
         val jsonNode = deserializationContext.readTree(jsonParser)
 
         return if (jsonNode.isArray()) {
+            if (jsonNode.size() > 1) {
+                log.warn("arbeidiflereland-array har {} elementer, kun første element brukes", jsonNode.size())
+            }
             deserializationContext.readTreeAsValue(jsonNode.iterator().next(), ArbeidIFlereLand::class.java)
         } else {
             deserializationContext.readTreeAsValue(jsonNode, ArbeidIFlereLand::class.java)
